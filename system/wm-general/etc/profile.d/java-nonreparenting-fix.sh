dfcadd2ef138c36d17098a79d53e80bd041d112e
Robbert van der Helm <robbert@zzindi.nl>
2017-02-04 14:07:47 +0100

    Disable the java nonreparenting WM fix
    This is not needed in i3 and messes up the HiDPI detection in JetBrains
    software.

--- system/wm-general/etc/profile.d/java-nonreparenting-fix.sh
+++ system/wm-general/etc/profile.d/java-nonreparenting-fix.sh
@@ -1,3 +1,6 @@
 # Fix applications not receiving size updates when using a non reparenting
 # window manager
-export _JAVA_AWT_WM_NONREPARENTING=1
+
+# Re-enable this for bspwm
+# i3 is a reparenting WM so it's not required, and it messes with HiDPI
+#export _JAVA_AWT_WM_NONREPARENTING=1
