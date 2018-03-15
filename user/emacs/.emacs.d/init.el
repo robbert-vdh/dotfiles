c16824e965ffc29af9691401de2adf27a2d5b295
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-03-15 11:11:29 +0100

    Enable the magit module
    Magit used to be part of the version control module.

--- user/emacs/.emacs.d/init.el
+++ user/emacs/.emacs.d/init.el
@@ -77,6 +77,7 @@
       ;impatient-mode    ; show off code over HTTP
       ;macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
+       magit             ;
        neotree           ; a project drawer, like NERDTree for vim
       ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
