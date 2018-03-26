b37e0a6880f85594ee0b8ab2e06dd2f779d8de35
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-03-26 11:27:50 +0200

    Use regular counsel-rg again
    This allows for easy searching in other directories.

--- user/emacs/.config/doom/+bindings.el
+++ user/emacs/.config/doom/+bindings.el
@@ -6,12 +6,12 @@
 (map!
  [remap dabbrev-expand] #'hippie-expand
  [remap newline]        #'+robbert/newline-and-indent
- :i [M-return]          #'newline-and-indent  ;; The default is adviced to continue comments
+ :i [M-return]          #'newline-and-indent ;; The default is adviced to continue comments
 
  :ni "C-S-SPC"          #'company-yasnippet
- :ne [(shift meta f)]   #'+ivy/project-search ;; As a complement to the `M-f' Swiper defined in +defualt
- :nvie "M-q"            #'fill-paragraph      ;; Doom Emacs overrides this to quit by default
- :v "C-u"               #'evil-scroll-up      ;; `evil-want-C-u-scroll' doesn't do anything in visual mode
+ :ne [(shift meta f)]   #'counsel-rg         ;; As a complement to the `M-f' Swiper defined in +defualt
+ :nvie "M-q"            #'fill-paragraph     ;; Doom Emacs overrides this to quit by default
+ :v "C-u"               #'evil-scroll-up     ;; `evil-want-C-u-scroll' doesn't do anything in visual mode
 
  :m "[a"                #'+robbert/languagetool-previous-error
  :m "]a"                #'+robbert/languagetool-next-error
@@ -53,7 +53,7 @@
      :desc "Ripgrep in project"      :nv "/" #'counsel-projectile-rg)
 
    (:desc "search"                   :prefix "/"
-     :desc "Project"                 :nv "/" #'+ivy/project-search
+     :desc "Project"                 :nv "/" #'counsel-rg
      :desc "Find in directory"       :nv "f" #'+robbert/find-file-in-dir
      :desc "Find in project"         :nv "p" #'+robbert/find-file-in-project
      :desc "Buffer"                  :nv "s" #'swiper)
