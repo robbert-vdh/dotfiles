82764aad4bbb9bbf55d2f1a8ad4dc91fd880820b
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-05-16 14:12:04 +0200

    Mark a dired keybinding as broken

--- user/emacs/.config/doom/+bindings.el
+++ user/emacs/.config/doom/+bindings.el
@@ -183,7 +183,7 @@
      :nv "C-l"  nil))
  (:after dired
    (:map dired-mode-map
-     :n "SPC"   nil
+     :n "SPC"   nil ;; FIXME
      :n "["     nil
      :n "]"     nil
      :n "{"     #'dired-prev-dirline
