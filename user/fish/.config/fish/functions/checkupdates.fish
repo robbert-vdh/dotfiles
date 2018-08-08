b4ec38da34bd9d68b3c00f651da6503282629032
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-08-09 01:14:44 +0200

    Don't highlight changed package revisiosn

--- user/fish/.config/fish/functions/checkupdates.fish
+++ user/fish/.config/fish/functions/checkupdates.fish
@@ -16,8 +16,8 @@ function __version_diff -d 'Highlight lines with changed major/minor ver'
     perl -pe 's/\x1b\[[^\x1b]*?m//g' | gawk '{
             # If a package changes its major/minor version, highlight it in
             # green
-            match($2, /^[^.]+\.[^.[:alpha:]]+/, old_version)
-            match($4, /^[^.]+\.[^.[:alpha:]]+/, new_version)
+            match($2, /^[^.]+\.[^[:alpha:].-]+/, old_version)
+            match($4, /^[^.]+\.[^[:alpha:].-]+/, new_version)
 
             if (old_version[0] != new_version[0]) {
                 print "\x1b[32m" $0 "\x1b[0m"
