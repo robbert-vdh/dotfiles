7d4928f5a82551311537c1a840e926b90e9f5e10
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-09-29 16:45:50 +0200

    Disable dashboard again
    This still messes up deleting workspaces.

--- user/emacs/.config/doom/init.el
+++ user/emacs/.config/doom/init.el
@@ -29,7 +29,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       ;; doom-dashboard    ; a nifty splash screen for Emacs
        doom-modeline     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
