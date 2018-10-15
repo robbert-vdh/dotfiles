81b7eb959ad94cb85559f5a683c6ea5d0cd8bfe2
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-10-15 23:38:43 +0200

    Get rid of everything i3 related, again

--- system/wm/etc/profile.d/jre.sh
+++
@@ -1,4 +0,0 @@
-# Do not change this unless you want to completely by-pass Arch Linux' way
-# of handling Java versions and vendors. Instead, please use script `archlinux-java`
-export PATH=${PATH}:/usr/lib/jvm/default/bin
-export _JAVA_OPTIONS="$_JAVA_OPTIONS -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
--- user/kde/.local/share/konsole/Profiel 1.profile
+++ user/kde/.local/share/konsole/Profiel 1.profile
@@ -2,7 +2,7 @@
 AntiAliasFonts=true
 BoldIntense=true
 ColorScheme=base16-tomorrow-night
-Font=Liberation Mono for Powerline,12,-1,5,50,0,0,0,0,0,Regular
+Font=Monospace,12,-1,5,50,0,0,0,0,0,Regular
 LineSpacing=0
 UseFontLineChararacters=false
 
