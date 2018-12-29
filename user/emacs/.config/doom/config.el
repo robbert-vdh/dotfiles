8ab6e55282fe3b86db2fe570e1e47fc0b6a2021e
Robbert van der Helm <mail@robbertvanderhelm.nl>
2018-12-29 21:22:39 +0100

    Show dotfiles in projectile

--- user/emacs/.config/doom/config.el
+++ user/emacs/.config/doom/config.el
@@ -187,6 +187,12 @@
   (dolist (mode (list magit-revision-mode-map))
     (define-key mode (kbd doom-leader-key) nil)))
 
+(after! projectile
+  ;; fd ignores dotfiles by default, but we don't want this in our repositories
+  (when (executable-find "fd")
+    (setq projectile-git-command "fd . --type f --hidden --exclude .git -0"
+          projectile-generic-command projectile-git-command)))
+
 (after! python
   ;; Set this to `django' to force docstring to always be on multiple lines
   (setq python-fill-docstring-style 'onetwo)
