9a0ee00147b697310372bca3da42892999111f2a
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-29 17:34:31 +0100

    Reenable Doom's dashboard
    Seems to be much more stable now.

--- user/emacs/.config/doom/init.el
+++ user/emacs/.config/doom/init.el
@@ -25,7 +25,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       ;; doom-dashboard    ; a nifty splash screen for Emacs
+       doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
        ;;fci               ; a `fill-column' indicator
