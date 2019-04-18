2255c8dcf88db3ff1e5c3d17b77afeb51a6b20d6
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-04-18 14:46:07 +0200

    Disown the chromium process in the jitsi script

--- user/fish/.config/fish/functions/jitsi.fish
+++ user/fish/.config/fish/functions/jitsi.fish
@@ -2,5 +2,5 @@ function jitsi -d "Open a Jitsi call in a new chromium instance"
     set url "https://meet.jit.si/$argv[1]"
 
     echo $url
-    chromium --app=$url > /dev/null 2>/dev/null &
+    chromium --app=$url > /dev/null 2>/dev/null &; disown
 end
