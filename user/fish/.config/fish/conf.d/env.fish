ff4dfd7553e7841125c25d12321324821664f884
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-09-21 15:00:37 +0200

    Always compile everything in parallel

--- user/fish/.config/fish/conf.d/env.fish
+++ user/fish/.config/fish/conf.d/env.fish
@@ -3,9 +3,12 @@ set -gx VISUAL $EDITOR
 set -gx PAGER less
 set -gx BROWSER firefox
 
-set -gx RUST_SRC_PATH "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
 # Let Firefox (and other compatible applications) use KDE dialogs
 set -gx GTK_USE_PORTAL 1
+# Always build with 8 threads by default
+set -gx MAKEFLAGS "-j8"
+# Needed for some tooling
+set -gx RUST_SRC_PATH "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
 # Always enable the esync patches in Wine
 set -gx WINEESYNC 1
 
