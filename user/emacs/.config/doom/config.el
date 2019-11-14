1c7be1282229cdc251b97231057f8ab392743ba4
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-11-14 09:29:30 +0100

    Fix face backtgrounds in doom-themes

--- user/emacs/.config/doom/config.el
+++ user/emacs/.config/doom/config.el
@@ -70,6 +70,70 @@
   ;; necesary when using emmet
   (remhash 'web-mode yas--parents))
 
+;; HACK: Fix doom-themes not yet respecting the :extend attribute, should be
+;;       removed once this gets fixed upstream
+(after! doom-themes
+  (after! org
+    (dolist (face '(org-block
+                    org-block-begin-line
+                    org-block-end-line
+                    org-level-1))
+      (set-face-extend face t)))
+  (after! magit
+    (dolist (face '(magit-diff-hunk-heading
+                    magit-diff-hunk-heading-highlight
+                    magit-diff-hunk-heading-selection
+                    magit-diff-hunk-region
+                    magit-diff-lines-heading
+                    magit-diff-lines-boundary
+                    magit-diff-conflict-heading
+                    magit-diff-added
+                    magit-diff-removed
+                    magit-diff-our
+                    magit-diff-base
+                    magit-diff-their
+                    magit-diff-context
+                    magit-diff-added-highlight
+                    magit-diff-removed-highlight
+                    magit-diff-our-highlight
+                    magit-diff-base-highlight
+                    magit-diff-their-highlight
+                    magit-diff-context-highlight
+                    magit-diff-whitespace-warning
+                    magit-diffstat-added
+                    magit-diffstat-removed
+                    magit-section-heading
+                    magit-section-heading-selection
+                    magit-section-highlight
+                    magit-section-secondary-heading
+                    magit-diff-file-heading
+                    magit-diff-file-heading-highlight
+                    magit-diff-file-heading-selection))
+      (set-face-extend face t)))
+  (after! ediff
+    (dolist (face '(ediff-current-diff-A
+                    ediff-current-diff-Ancestor
+                    ediff-current-diff-B
+                    ediff-current-diff-C
+                    ediff-even-diff-A
+                    ediff-even-diff-Ancestor
+                    ediff-even-diff-B
+                    ediff-even-diff-C
+                    ediff-fine-diff-A
+                    ediff-fine-diff-Ancestor
+                    ediff-fine-diff-B
+                    ediff-fine-diff-C
+                    ediff-odd-diff-A
+                    ediff-odd-diff-Ancestor
+                    ediff-odd-diff-B
+                    ediff-odd-diff-C))
+      (set-face-extend face t)))
+  (after! hl-line
+    (set-face-extend 'hl-line t))
+  (after! faces
+    (dolist (face '(region secondary-selection))
+      (set-face-extend face t))))
+
 (after! emmet-mode
   ;; Don't put an XML style slash at the end of self closing tags
   (setq emmet-self-closing-tag-style ""))
