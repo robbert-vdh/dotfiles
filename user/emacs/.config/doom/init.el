cbd7ff26b465d7849d883b2dabfdf959a96fb4b6
Robbert van der Helm <mail@robbertvanderhelm.nl>
2020-01-13 15:27:26 +0100

    Move Doom's config to a literate Org file

--- user/emacs/.config/doom/init.el
+++ user/emacs/.config/doom/init.el
@@ -171,7 +171,7 @@
        :config
        ;; For literate config users. This will tangle+compile a config.org
        ;; literate config in your `doom-private-dir' whenever it changes.
-       ;;literate
+       literate
 
        ;; The default module sets reasonable defaults for Emacs. It also
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
