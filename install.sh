4b7712556a923ab030fd8c5bbf2478ed408b97d8
Robbert van der Helm <mail@robbertvanderhelm.nl>
2020-08-21 12:23:10 +0200

    Fix installation script
    Stow changed its output to only print to stderr, and the 'install
    everything' option wasn't working properly.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # Source https://gist.github.com/davejamesmiller/1965569
 ask() {
   # http://djm.me/ask
-  local prompt default all REPLY
+  local prompt default REPLY
 
   while true; do
     if [ "${2:-}" = 'Y' ]; then
@@ -21,46 +21,39 @@ ask() {
       default=
     fi
 
-    if [ "${3:-}" = 'all' ]; then
-      all=true
-      prompt+='/a(ll)'
-    fi
-
     # Ask the question (not using "read -p" as it uses stderr not stdout)
     echo -n "$1 [$prompt] "
 
     # Read the answer (use /dev/tty in case stdin is redirected from somewhere
     # else)
-    read REPLY </dev/tty
+    read -r REPLY </dev/tty
 
     # Default?
-    if [ -z $REPLY ]; then
+    if [ -z "$REPLY" ]; then
       REPLY=$default
     fi
 
     # Check if the reply is valid
     case $REPLY in
-      Y*|y*) return 0 ;;
-      N*|n*) return 1 ;;
+    Y* | y*) return 0 ;;
+    N* | n*) return 1 ;;
     esac
   done
 }
 
 cd "$(dirname "$0")"
 
-git submodule init > /dev/null
-git submodule update > /dev/null
+git submodule init >/dev/null
+git submodule update >/dev/null
 
 if ask 'Dry run?' Y; then
-  DRY_RUN=true
+  DRY_RUN=1
 else
-  DRY_RUN=false
-
   set -e
   set -o pipefail
 
   if ask 'Install everything?' N; then
-    NO_ASK=true
+    NO_ASK=1
   fi
 fi
 
@@ -72,40 +65,41 @@ function stow2() {
     command="stow $1 -t $2 -v"
   fi
 
-  if [[ $2 == '/' ]]; then
+  if [[ -z "$DRY_RUN" && $2 == '/' ]]; then
     command="sudo $command"
   fi
 
-  if $DRY_RUN; then
-    command+=' --no 2>/dev/null'
-    eval $command
+  if [[ -n "$DRY_RUN" ]]; then
+    command+=" --no 2>&1 \
+      | grep -v 'WARNING: in simulation mode so not modifying filesystem.'"
+    eval "$command"
   else
-    if $NO_ASK || ask "Install configuration for $package?" Y; then
-      eval $command
+    if [[ -n "$NO_ASK" ]] || ask "Install configuration for $package?" Y; then
+      eval "$command"
     fi
   fi
 }
 
-if ! $DRY_RUN; then
+if [[ -z "$DRY_RUN" ]]; then
   echo 'Installing configuration...'
 fi
 
 cd user
-for package in $(ls); do
-  if [ -d $package ]; then
-    stow2 $package $HOME
+for package in *; do
+  if [ -d "$package" ]; then
+    stow2 "$package" "$HOME"
   fi
 done
 
-if ! $DRY_RUN; then
+if [[ -z "$DRY_RUN" ]]; then
   echo ''
   echo 'Installing systemm wide configuration...'
   echo 'NOTE: Misconfiguration could mess up your system'
 fi
 
 cd ../system
-for package in $(ls); do
-  if [ -d $package ]; then
-    stow2 $package '/'
+for package in *; do
+  if [ -d "$package" ]; then
+    stow2 "$package" '/'
   fi
 done
