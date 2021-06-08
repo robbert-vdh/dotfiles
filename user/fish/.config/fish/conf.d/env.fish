808df2ef042fa37b87b66c76ccc8a5ec99253759
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-06-08 18:01:57 +0200

    Append instead of prepend the CUDA path
    To avoid conflicts with GCC.

--- user/fish/.config/fish/conf.d/env.fish
+++ user/fish/.config/fish/conf.d/env.fish
@@ -23,8 +23,9 @@ for install_dir in (flatpak --installations)
     set -gxa XDG_DATA_DIRS $install_dir/exports/share
 end
 
-# Neither do the binaries from the CUDA package
-set -ga fish_user_paths /opt/cuda/bin
+# Neither do the binaries from the CUDA package. These need to be appended to
+# the path since CUDA includes an old version of GCC.
+set -gx PATH "$PATH:/opt/cuda/bin"
 
 # These should also be here
 set -gxa XDG_DATA_DIRS /usr/local/share /usr/share /var/lib/snapd/desktop
