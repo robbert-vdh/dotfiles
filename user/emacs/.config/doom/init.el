ed627c5822848b28616918162cbd110de3836b85
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-08-10 12:36:27 +0200

    Disable using popups for everything
    This seems to clash with embark.

--- user/emacs/.config/doom/init.el
+++ user/emacs/.config/doom/init.el
@@ -35,7 +35,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
+        ;;+all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        ;;tabs              ; FIXME an (incomplete) tab bar for Emacs
        (treemacs +lsp)   ; a project drawer, like neotree but cooler
