2c2d84477cdd03a1b9613ec2ca1a04a2ef61c592
Robbert van der Helm <mail@robbertvanderhelm.nl>
2021-12-14 04:26:45 +0100

    Swap out stock media session for wireplumber

---
+++ user/pipewire/.config/wireplumber/main.lua.d/90-alsa-config.lua
@@ -0,0 +1,30 @@
+-- See /usr/share/wireplumber/main.lua.d/50-alsa-config.lua for all properties,
+-- this just overrides some settings for low latency USB audio
+
+alsa_monitor.rules[#alsa_monitor.rules + 1] = {
+  matches = {
+    {
+      { "node.name", "matches", "alsa_input.*" },
+    },
+    {
+      { "node.name", "matches", "alsa_output.*" },
+    },
+  },
+  apply_properties = {
+    ["audio.rate"]             = 44100,
+    ["api.alsa.period-size"]   = 20,
+    ["api.alsa.disable-batch"] = true,
+  },
+}
+
+alsa_monitor.rules[#alsa_monitor.rules + 1] = {
+  matches = {
+    {
+      { "node.name", "matches", "alsa_*.usb-*" },
+    },
+  },
+  apply_properties = {
+    -- USB audio interfaces can take a while to wake up from suspending
+    ["session.suspend-timeout-seconds"] = 0,
+  },
+}
