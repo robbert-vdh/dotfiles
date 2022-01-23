fa4202163ece5bfb99f229774aaf91e2c5782958
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-01-23 20:25:30 +0100

    Disable ALSA period num setting
    This just seems to do weird things, not sure if it's working properly
    yet.

--- user/pipewire/.config/wireplumber/main.lua.d/90-alsa-config.lua
+++ user/pipewire/.config/wireplumber/main.lua.d/90-alsa-config.lua
@@ -12,12 +12,12 @@ alsa_monitor.rules[#alsa_monitor.rules + 1] = {
   },
   apply_properties = {
     ["audio.rate"]             = 44100,
-    ["api.alsa.period-size"]   = 64,
+    ["api.alsa.period-size"]   = 32,
     -- This is super weird right now, not sure why these super high values are
     -- required here when ALSA applications or JACK2 can normally do 64/3
     -- frames/periods. Need to investigate this further after a couple PipeWire
     -- updates.
-    ["api.alsa.period-num"]    = 256,
+    ["api.alsa.period-num"]    = 0,
     ["api.alsa.disable-batch"] = true,
   },
 }
