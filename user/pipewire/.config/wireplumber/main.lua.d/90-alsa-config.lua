5bcc81c0455ca4982e12161d24bafd5fefe8e2a5
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-04-26 17:11:16 +0200

    Revert the Pipewire buffer settings
    These old values seem to work again, the optimal configuration is still
    a mystery.

--- user/pipewire/.config/wireplumber/main.lua.d/90-alsa-config.lua
+++ user/pipewire/.config/wireplumber/main.lua.d/90-alsa-config.lua
@@ -12,7 +12,8 @@ alsa_monitor.rules[#alsa_monitor.rules + 1] = {
   },
   apply_properties = {
     ["audio.rate"]             = 44100,
-    ["api.alsa.period-size"]   = 64,
+    -- ["api.alsa.period-size"]   = 64,
+    ["api.alsa.period-size"]   = 24,
     -- This is super weird right now, not sure why these super high values are
     -- required here when ALSA applications or JACK2 can normally do 64/3
     -- frames/periods. Need to investigate this further after a couple PipeWire
