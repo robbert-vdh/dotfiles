f2ee4a564116bb42ff789163f89e051a8e6a8134
Robbert van der Helm <mail@robbertvanderhelm.nl>
2023-05-18 18:48:11 +0200

    Add more Python packages to Home Manager

--- user/home-manager/.config/home-manager/home.nix
+++ user/home-manager/.config/home-manager/home.nix
@@ -1,5 +1,24 @@
 { config, pkgs, ... }:
 
+# These Python packages aren't packaged in nixpkgs
+let mkJupyterlab_code_formatter = ps: with ps; (buildPythonPackage rec {
+      pname = "jupyterlab_code_formatter";
+      version = "2.0.0";
+      format = "pyproject";
+
+      src = fetchPypi {
+        inherit pname version;
+        sha256 = "gGWqAPLmes6PqdKLYq1NPow23uL3TlOg0SKkHq0ixew=";
+      };
+
+      buildInputs = [
+        hatchling
+        hatch-jupyter-builder
+        hatch-nodejs-version
+        jupyterlab
+      ];
+    });
+in
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -22,7 +41,8 @@
     # theory.
     (pkgs.emacs28.override { withPgtk = true; })
 
-    # Important tools
+    # Important tools. I'm not installing fish through Nix since I'm using it as
+    # my login shell, and it needs some config to know about Nix.
     pkgs.git
 
     # Useful to have available globally of specific projects
@@ -38,11 +58,47 @@
     pkgs.ripgrep
     pkgs.websocat
 
-    # Python programs
-    pkgs.python310Packages.ipython
+    ((pkgs.python310.override {
+      packageOverrides = self: super: {
+        # Neither Qt nor TK works out of the box. I figured out a workaround to
+        # get the Qt backend to work involving the `qt5-run` wrapper defined
+        # below, so that's what I'm using for now.
+        matploblib = super.matplotlib.override { enableQt = true; enableTk = false; };
+      };
+    }).withPackages (ps: with ps; [
+      # Executables
+      ipython
+      jupyterlab
 
-    # Haskell programs
+      # Formatters and linters
+      black
+      (mkJupyterlab_code_formatter ps)
+      isort
+
+      # Libs
+      librosa
+      matplotlib
+      pandas
+      pyqt5 # Needed for matplotlib
+      scipy
+      seaborn
+      statsmodels
+    ]))
+
     pkgs.haskellPackages.ghc-events
+
+    # HACK: Workaround needed to set the correct Qt plugin path so matplotlib
+    #       works in IPython:
+    #       https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
+    (pkgs.writeShellScriptBin "qt5-run" (with pkgs.qt5; ''
+      export QT_PLUGIN_PATH=${qtbase}/${qtbase.qtPluginPrefix}
+      if [[ $# -eq 0 ]]; then
+        echo >&2 "Usage: qt5-run <command...>"
+        exit 1
+      fi
+
+      exec "$@"
+    ''))
 
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
