54d3bd947c3d1ed7b8823fb4d0a0553dc2b76af7
Robbert van der Helm <mail@robbertvanderhelm.nl>
2023-05-29 15:33:24 +0200

    Print a message the first time requesting sudo

--- modules/pacman/default.nix
+++ modules/pacman/default.nix
@@ -6,12 +6,21 @@
     # requires sudo, but it doesn't do anything if the files in /etc are already
     # symlinks to the correct files.
     symlinkPacmanConfigs = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      # We'll print a message the first time we require sudo here
+      have_symlinked=0
+
       symlink_if_needed() {
         local filename target_path
 
         filename="$(basename "$1")"
         target_path="/etc/$filename"
         if ! diff --brief "$target_path" "$1" >/dev/null 2>&1; then
+          if [[ $have_symlinked -eq 0 ]]; then
+            echo "Symlinking pacman config files to /etc..."
+            have_symlinked=1
+          fi
+
+          # This needs to use /usr/bin/sudo directly
           $DRY_RUN_CMD /usr/bin/sudo ln -sf $VERBOSE_ARG "$1" "$target_path"
         fi
       }
