2264af07be0e98ad8aa645db574004e5f8974f99
Robbert van der Helm <robbert.vanderhelm@channable.com>
2023-07-07 11:23:55 +0200

    Use Nix Python for the system gcloud cli

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -24,50 +24,59 @@ let
       buildInputs =
         [ hatchling hatch-jupyter-builder hatch-nodejs-version jupyterlab ];
     });
-in {
-  home.packages = [
-    ((pkgs.python310.override {
-      packageOverrides = self: super: {
-        # Neither Qt nor TK works out of the box. I figured out a workaround to
-        # get the Qt backend to work involving the `qt5-run` wrapper defined
-        # below, so that's what I'm using for now.
-        matploblib = super.matplotlib.override {
-          enableQt = true;
-          enableTk = false;
-        };
 
-        ipython = super.ipython.overridePythonAttrs (old: {
-          # HACK: Workaround needed to set the correct Qt plugin path so
-          #       matplotlib works in IPython:
-          #       https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
-          makeWrapperArgs = (old.makeWrapperArgs or [ ])
-            ++ qtPluginPathMakeWrapperArgs;
-        });
+  pythonEnv = ((pkgs.python310.override {
+    packageOverrides = self: super: {
+      # Neither Qt nor TK works out of the box. I figured out a workaround to
+      # get the Qt backend to work involving the `qt5-run` wrapper defined
+      # below, so that's what I'm using for now.
+      matploblib = super.matplotlib.override {
+        enableQt = true;
+        enableTk = false;
       };
-    }).withPackages (ps:
-      with ps; [
-        # Binaries
-        pgcli
-        ipython
-        jupyterlab
 
-        # Formatters and linters
-        black
-        (mkJupyterlab_code_formatter ps)
-        isort
+      ipython = super.ipython.overridePythonAttrs (old: {
+        # HACK: Workaround needed to set the correct Qt plugin path so
+        #       matplotlib works in IPython:
+        #       https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
+        makeWrapperArgs = (old.makeWrapperArgs or [ ])
+          ++ qtPluginPathMakeWrapperArgs;
+      });
+    };
+  }).withPackages (ps:
+    with ps; [
+      # Binaries
+      pgcli
+      ipython
+      jupyterlab
 
-        # Libs
-        librosa
-        matplotlib
-        numpy
-        pandas
-        pyperclip
-        pyqt5 # Needed for matplotlib
-        scipy
-        seaborn
-        statsmodels
-      ]))
-  ];
+      # Formatters and linters
+      black
+      (mkJupyterlab_code_formatter ps)
+      isort
+
+      # Libs
+      librosa
+      matplotlib
+      numpy
+      pandas
+      pyperclip
+      pyqt5 # Needed for matplotlib
+      scipy
+      seaborn
+      statsmodels
+    ]));
+in {
+  home.packages = [ pythonEnv ];
+
+  # The system installed Google Cloud environment cannot find NumPy by default
+  # since it uses its own bundled Python interpreter. As a workaround we can
+  # tell it to use our own Python interpreter which does include NumPy. It will
+  # be fine!
+  home.sessionVariables = {
+    CLOUDSDK_PYTHON_SITEPACKAGES = 1;
+    CLOUDSDK_PYTHON = "${pythonEnv}/bin/python3";
+  };
 
   xdg.configFile."flake8".source = ./flake8;
   xdg.configFile."pylintrc".source = ./pylintrc;
