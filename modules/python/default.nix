82abb5b45ff25e212bac179cc60464fde21dbfea
Robbert van der Helm <mail@robbertvanderhelm.nl>
2023-09-17 14:20:37 +0200

    Remove matplotlib qt hack

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 
 let
-  # See below
-  qtPluginPathMakeWrapperArgs = with pkgs.qt5; [
-    "--set"
-    "QT_PLUGIN_PATH"
-    "${qtbase}/${qtbase.qtPluginPrefix}"
-  ];
-
   # These Python packages aren't packaged in nixpkgs
   mkJupyterlab_code_formatter = ps:
     with ps;
@@ -25,25 +18,8 @@ let
         [ hatchling hatch-jupyter-builder hatch-nodejs-version jupyterlab ];
     });
 
-  pythonEnv = ((pkgs.python310.override {
-    packageOverrides = self: super: {
-      # Neither Qt nor TK works out of the box. I figured out a workaround to
-      # get the Qt backend to work involving the `qt5-run` wrapper defined
-      # below, so that's what I'm using for now.
-      matploblib = super.matplotlib.override {
-        enableQt = true;
-        enableTk = false;
-      };
-
-      ipython = super.ipython.overridePythonAttrs (old: {
-        # HACK: Workaround needed to set the correct Qt plugin path so
-        #       matplotlib works in IPython:
-        #       https://github.com/NixOS/nixpkgs/issues/80147#issuecomment-784857897
-        makeWrapperArgs = (old.makeWrapperArgs or [ ])
-          ++ qtPluginPathMakeWrapperArgs;
-      });
-    };
-  }).withPackages (ps:
+  # git-blame this for a wrapper that fixes matplotlib+Qt
+  pythonEnv = pkgs.python310.withPackages (ps:
     with ps; [
       # Binaries
       ipython
@@ -61,11 +37,10 @@ let
       numpy
       pandas
       pyperclip
-      pyqt5 # Needed for matplotlib
       scipy
       seaborn
       statsmodels
-    ]));
+    ]);
 in {
   home.packages = [ pythonEnv pkgs.nbstripout ];
 
