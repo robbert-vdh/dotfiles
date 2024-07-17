d254e267b0ce0bd584db6c98e16311084c8d94bd
Robbert van der Helm <robbert.vanderhelm@channable.com>
2024-07-17 09:33:26 +0200

    Use external fourmolu for HLS
    This lets it use our custom fourmolu config.

--- modules/haskell/default.nix
+++ modules/haskell/default.nix
@@ -1,10 +1,3 @@
 { pkgs, ... }:
 
-{
-  home.packages = [
-    pkgs.haskellPackages.implicit-hie # Useful to have available globally of specific projects
-    pkgs.haskellPackages.ghc-events # Useful for investigating RTS event logs
-  ];
-
-  xdg.configFile."fourmolu.yaml".source = ./fourmolu.yaml;
-}
+{ }
