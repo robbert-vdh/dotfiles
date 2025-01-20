1978ddbe0f2b2afa4da1780c73fdbe1420880004
Robbert van der Helm <robbert.vanderhelm@channable.com>
2025-01-20 12:12:52 +0100

    Enable better syntax highlighting for web-mode

--- modules/emacs/doom/init.el
+++ modules/emacs/doom/init.el
@@ -173,7 +173,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       (web +lsp +treesitter)               ; the tubes
        (yaml +lsp)         ; JSON, but readable
        ;;zig               ; C, but simpler
 
