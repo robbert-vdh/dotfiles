37a45c06a4f9309f8d0a24adb7b26d91fbe9f449
Robbert van der Helm <robbert.vanderhelm@channable.com>
2026-01-13 10:58:17 +0100

    Enable lsp-treemacs

--- modules/emacs/doom/init.el
+++ modules/emacs/doom/init.el
@@ -48,7 +48,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       (treemacs +lsp)          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
